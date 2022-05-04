5db90767c543c5be73eca3dcf9b713b247853052
Simon Grünbacher <simon.gruenbacher@gmail.com>
2022-05-04 10:15:28 +0200

    removed comments work in progress: get key navigation working

--- main.ts
+++ main.ts
@@ -1,103 +1,35 @@
-import { waitForDebugger } from 'inspector';
-import { App, Editor, editorEditorField, EditorPosition, finishRenderMath, ItemView, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, renderMath, Setting, Workspace, WorkspaceLeaf } from 'obsidian';
-import { cursorTo } from 'readline';
+import { Editor, ItemView, MarkdownView, Plugin, renderMath, WorkspaceLeaf } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-/*
-interface MyPluginSettings {
-	mySetting: string;
-} 
-*/
-
-/*
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-*/
 
 const LatexContextViewType = 'latex-context-view'
 
 export default class MyPlugin extends Plugin {
 	//settings: MyPluginSettings;
 
-	//latexContextView : LatexContextView;
+	latexContextView : LatexContextView;
 	latexLeaf : WorkspaceLeaf;
 
 	async onload() {
 		//await this.loadSettings();
 
-		this.registerView(LatexContextViewType, leaf => (/*this.latexContextView = */new LatexContextView(leaf)));
+		this.registerView(LatexContextViewType, leaf => (this.latexContextView = new LatexContextView(leaf)));
 
-		/*
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.registerScopeEvent(this.app.scope.register([], 'ArrowUp', (event, context) => {
+			if(this.latexContextView) {
+				this.latexContextView.changeFocus(0, -1);
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		}));
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-		*/
 		this.addCommand({
-			id: 'open-latex-overlay',
-			name: 'Open Latex Overlay',
+			id: 'open-latex-leaf',
+			name: 'Open Latex Leaf',
 			editorCallback: (editor: Editor, view : MarkdownView) => {
 				if(this.latexLeaf) {
 					this.app.workspace.setActiveLeaf(this.latexLeaf);
+					return;
 				} // only spawn a new view if none is available
-				//console.log('Active line:' + editor.getCursor('from'));
-				//editor.setLine(editor.getCursor('from').line, 'you got hacked');
-				/*const cursorPosition = editor.getCursor('head');
-				var lineNr = cursorPosition.line;
-				var column = cursorPosition.ch;
-				var line = editor.getLine(lineNr);
-				var newLine = line.substring(0, column) + 'test' + line.substring(column);
-				
-				//console.log('from: ' + editor.getCursor('from').line + ', ' + editor.getCursor('from').ch + 'to: ' + editor.getCursor('to').line + ', ' + editor.getCursor('to').ch + 'anchor: ' + editor.getCursor('anchor').line + ', ' + editor.getCursor('anchor').ch +'head: ' + editor.getCursor('head').line + ', ' + editor.getCursor('head').ch );
-				editor.setLine(lineNr, newLine);
-				editor.setCursor(cursorPosition);
-				*/
 
 				this.latexLeaf = this.app.workspace.getRightLeaf(false);
 				this.app.workspace.revealLeaf(this.latexLeaf)
@@ -117,39 +49,38 @@ export default class MyPlugin extends Plugin {
 			]
 		});
 
-		/*
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-		*/
 	}
 
 	onunload() {
 		this.latexLeaf.detach();
 	}
-
-	/*
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		//await this.saveData(this.settings);
-	}
-	*/
 }
 
 class LatexContextView extends ItemView {
 	// see https://github.com/tgrosinger/advanced-tables-obsidian/blob/28a0a65f71d72666a5d0c422b5ed342bbd144b8c/src/table-controls-view.ts
 	editor : Editor;
+	buttons : HTMLElement[][];
+
+	private focusedRow = -1;
+	private focusedCol = -1;
+
+	static LINE_WIDTH = 4; // number of commands per table line
+
+	changeFocus(dx : number, dy : number) {
+		console.log('changing focus');
+		if(0 <= this.focusedRow 
+			&& this.focusedRow < this.buttons.length 
+			&& 0 <= this.focusedCol 
+			&& this.focusedCol < this.buttons[this.focusedRow].length) {
+			this.buttons[this.focusedRow][this.focusedCol].setAttr('color', 'white');
+		}
+
+		this.focusedRow = (this.focusedRow + this.buttons.length + dy) % this.buttons.length;
+		this.focusedCol = (this.focusedCol + this.buttons[this.focusedRow].length + dx) % this.buttons[this.focusedRow].length;
+
+		this.buttons[this.focusedRow][this.focusedCol].setAttr('color', 'red');
+
+	}
 
 	getDisplayText(): string {
 		return 'TODO: rename display text';
@@ -161,58 +92,46 @@ class LatexContextView extends ItemView {
 
 	load() : void {
 		super.load();
-		//console.log('LatexContextView loaded');
-
-		const LINE_WIDTH = 2; // number of commands per table line
-		
 		const leaf = this.app.workspace.activeLeaf;
 		if(leaf.view instanceof MarkdownView) {
 			this.editor = leaf.view.editor;
 			insertText(this.editor, 'sample text')
 		} else {
 			console.warn('Unable to determine active Editor');
+			return;
 		}
+
+
+		this.buttons = [];
 
 		const container = this.containerEl.children[1];
 		container.empty();
 
-		const rootEl = container.createEl('div'); //document.createElement('div');
+		const rootEl = container.createEl('div');
 		const table = rootEl.createEl('table');
 
 		const remaining = GREEKS;
 		while(remaining.length > 0) {
-			const row = table.insertRow();
-			remaining.splice(0, LINE_WIDTH).forEach((command, i) => {
-				const cell = row.insertCell();
-				drawButton(command, cell, () => insertText(this.editor, `$${command}$`));
+			const tableRow = table.insertRow();
+			const buttonRow : HTMLElement[] = [];
+			remaining.splice(0, LatexContextView.LINE_WIDTH).forEach((command, i) => {
+				const cell = tableRow.insertCell();
+				buttonRow.push(drawButton(command, cell, () => insertText(this.editor, `$${command}$`)));
 			});
+			this.buttons.push(buttonRow);
 			//const row = remaining.splice(0, LINE_WIDTH);
 		}
-		//rootEl.appendChild(renderMath('\\dots', true));
-		//await finishRenderMath();
-		/*rootEl.onClickEvent(() => {
-			const leaf = this.app.workspace.activeLeaf;
-			if(leaf.view instanceof MarkdownView) {
-				const editor = leaf.view.editor;
-				insertText(editor, 'sample text')
-			} else {
-				console.warn('Unable to determine active Editor');
-			}
-		})*/
-		//container.empty();
-		//container.appendChild(rootEl);
 	}
-
-	
 }
 
-function drawButton(latexCommand : string, parent : HTMLElement, callback : () => any) : void {
+function drawButton(latexCommand : string, parent : HTMLElement, callback : () => any) : HTMLElement {
 	const button = parent.appendChild(renderMath(latexCommand, true));
 	button.onClickEvent(event => {
-		if(event.button == 0) {// main (left) mouse button
+		if(event.button == 0) { // main (left) mouse button
 			callback();
 		}
-	})
+	});
+	return button;
 }
 
 function insertText(editor : Editor, text : string) {
@@ -240,52 +159,3 @@ const MATH_OPERATORS : LatexCommandGroup = [
 	'\\cdot',
 	'\\oplus'
 ]
-
-/*
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-*/
-
-/*
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
-*/
