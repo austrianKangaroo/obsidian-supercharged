5c43af753f078b43fa3745c04a423ee06afc02c6
Simon Grünbacher <simon.gruenbacher@gmail.com>
2022-06-01 15:29:11 +0200

    add comments

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,34 @@
-import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting, WorkspaceLeaf } from 'obsidian';
+import { Editor, MarkdownView, Plugin, WorkspaceLeaf } from 'obsidian';
 import CanvasView from 'src/CanvasView';
 import LatexContextView from 'src/LatexContextView';
-import OSC_SettingTab from 'src/OSC_SettingTab';
+import { OSC_SettingTab, OSC_PluginSettings, DEFAULT_SETTINGS } from 'src/OSC_SettingTab';
 
-//gives the User the opportunity to choose their 5 latex codes for their interface
-interface OSC_PluginSettings {
-	custom_commands: string[];
-}
 
-const DEFAULT_SETTINGS: OSC_PluginSettings = {
-	custom_commands: [
-		'command',
-		'command',
-		'command',
-		'command',
-		'command'
-	]
-}
 
 
 export default class OSC_Plugin extends Plugin {
 	settings: OSC_PluginSettings;
 
 	latexLeaf: WorkspaceLeaf;
-	activeEditor: Editor;
-
 	canvasLeaf : WorkspaceLeaf;
 
-	async onload() { // this funtion gets executed once the plugin gets activated
-		await this.loadSettings();
+	activeEditor: Editor;
 
+	async onload() { // this funtion gets executed once the plugin gets activated
+		await this.loadSettings(); // loads the settings (custom commands)
+
+		// register a new view type to obsidian. 
+		//The callback gets executed whenever setViewState({type : LatexContextView.TYPE, ...}); gets called
 		this.registerView(LatexContextView.TYPE, leaf => new LatexContextView(this, leaf));
 
+		// register the command for opening the latex leaf.
+		// it can be executed under the specified name using the launchpad or the specified keyboard shortcut.
 		this.addCommand({
 			id: 'open-latex-leaf',
 			name: 'Open Latex Leaf',
+			// this callback gets called whenever the command is executed within an editor.
+			// requiring this is necessary because otherwise we have no way of knowing where to insert the latex commands
+			// since no other callbacks are provided, the command is a no-op if executed outside the editor
 			editorCallback: (editor: Editor, _view: MarkdownView) => {
 				this.spawnLatexView(editor);
 			},
@@ -48,6 +42,7 @@ export default class OSC_Plugin extends Plugin {
 			]
 		});
 
+		// see above for an explanation of registerView
 		this.registerView(CanvasView.TYPE, leaf => new CanvasView(this, leaf));
 		this.addCommand({
 			id: 'open-supercharged-canvas',
@@ -65,20 +60,25 @@ export default class OSC_Plugin extends Plugin {
 			]
 		})
 
-		//This adds a settings tab so the user can configure various aspects of the plugin
+		//This adds a tab to the settings menu so the user can configure various aspects of the plugin
 		this.addSettingTab(new OSC_SettingTab(this.app, this));
 	}
 
+	/*
+	This function gets executed when the plugin is disabled. 
+	Note that this function does not get executed when the obsidian app is closed
+	*/
 	onunload() {
 		this.latexLeaf?.detach();
 		this.canvasLeaf?.detach();
 	}
 
-
+	// read settings from data.json or fall back to default settings
 	private async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
+	// save current settings to data.json
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
