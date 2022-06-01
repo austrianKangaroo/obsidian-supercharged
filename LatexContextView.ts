62471ff81981c0b57b7ec9e98010249a67ab2a94
Simon Grünbacher <simon.gruenbacher@gmail.com>
2022-06-01 10:33:07 +0200

    move latex context view to seperate file

---
+++ LatexContextView.ts
@@ -0,0 +1,92 @@
+import { ItemView, WorkspaceLeaf, loadMathJax, finishRenderMath, renderMath } from 'obsidian';
+import MyPlugin from 'main';
+import { COMMAND_GROUPS } from 'latexCommands';
+
+export default class LatexContextView extends ItemView {
+	plugin: MyPlugin;
+    static LatexContextViewType = 'latex-context-view';
+
+	// see https://github.com/tgrosinger/advanced-tables-obsidian/blob/28a0a65f71d72666a5d0c422b5ed342bbd144b8c/src/table-controls-view.ts
+
+	constructor(plugin: MyPlugin, leaf: WorkspaceLeaf) {
+		super(leaf);
+		this.plugin = plugin;
+	}
+
+
+	getDisplayText(): string {
+		return 'Obsidian Supercharged - Latex';
+	}
+
+	getViewType(): string {
+		return LatexContextView.LatexContextViewType;
+	}
+
+	async onload(): Promise<void> {
+		await loadMathJax();
+
+		if (!this.plugin.activeEditor) {
+			/*
+			this happens if the app gets closed and reopened
+			in this case, we want to close the leaf as we have no access to an editor.
+			this means that our button callbacks can't know where to insert text
+			*/
+			this.leaf.detach();
+			return;
+		}
+
+		const container = this.contentEl;
+
+		const rootEl = container.createDiv({ cls: 'supercharged-table' });
+
+		const commandGroups = [{
+			name: 'custom commands',
+			commands: this.plugin.settings.custom_commands
+		}].concat(COMMAND_GROUPS);
+
+		commandGroups.forEach((group, i) => {
+			const groupDiv= rootEl.createDiv();
+			const header = groupDiv.createEl('h2',{cls:'collapsible'});
+			header.textContent = group.name;
+			const content = groupDiv.createDiv({cls:'content'});
+			group.commands.forEach((command, index) => {
+				drawButton(command, content, () => {
+					this.plugin.insertText(this.plugin.activeEditor, command);
+					this.plugin.activeEditor.focus(); // return focus to editor after the button has been pressed
+				});
+			})
+		});
+		collapse();
+		await finishRenderMath();
+	}
+
+	onunload(): void {
+		this.plugin.latexLeaf = null;
+		this.plugin.canvasLeaf = null;
+	}
+}
+
+function drawButton(latexCommand: string, parent: HTMLElement, callback: () => any): HTMLButtonElement {
+	const button = parent.createEl('button');
+	button.appendChild(renderMath(latexCommand, true));
+	button.onClickEvent(callback);
+	return button;
+}
+
+function collapse() {
+	var coll = document.getElementsByClassName("collapsible");
+	var i;
+	
+	for (i = 0; i < coll.length; i++) {
+	  coll[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+		if (content.style.maxHeight){
+			content.style.maxHeight = null;
+		} else {
+			content.style.maxHeight = 0 + "px";
+		//	content.style.maxHeight = content.scrollHeight + "px";
+		}
+	  });
+	}
+}
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { App, Editor, finishRenderMath, ItemView, loadMathJax, MarkdownView, Notice, Plugin, PluginSettingTab, renderMath, Setting, WorkspaceLeaf } from 'obsidian';
-import { COMMAND_GROUPS } from 'latexCommands';
 import { CanvasContextViewType, CanvasView } from 'canvas';
+import LatexContextView from 'LatexContextView';
 
 // Remember to rename these classes and interfaces!
 
@@ -19,24 +19,6 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	]
 }
 
-function collapse() {
-	var coll = document.getElementsByClassName("collapsible");
-	var i;
-	
-	for (i = 0; i < coll.length; i++) {
-	  coll[i].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var content = this.nextElementSibling;
-		if (content.style.maxHeight){
-			content.style.maxHeight = null;
-		} else {
-			content.style.maxHeight = 0 + "px";
-		//	content.style.maxHeight = content.scrollHeight + "px";
-		}
-	  });
-	}}
-
-const LatexContextViewType = 'latex-context-view'
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -49,7 +31,7 @@ export default class MyPlugin extends Plugin {
 	async onload() { // this funtion gets executed once the plugin gets activated
 		await this.loadSettings();
 
-		this.registerView(LatexContextViewType, leaf => new LatexContextView(this, leaf));
+		this.registerView(LatexContextView.LatexContextViewType, leaf => new LatexContextView(this, leaf));
 
 		this.addCommand({
 			id: 'open-latex-leaf',
@@ -65,7 +47,7 @@ export default class MyPlugin extends Plugin {
 				this.latexLeaf = this.app.workspace.getRightLeaf(false);
 				this.app.workspace.revealLeaf(this.latexLeaf)
 				this.latexLeaf.setViewState({
-					type: LatexContextViewType,
+					type: LatexContextView.LatexContextViewType,
 					active: true
 				});
 			},
@@ -135,75 +117,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class LatexContextView extends ItemView {
-	plugin: MyPlugin;
 
-	// see https://github.com/tgrosinger/advanced-tables-obsidian/blob/28a0a65f71d72666a5d0c422b5ed342bbd144b8c/src/table-controls-view.ts
-
-	constructor(plugin: MyPlugin, leaf: WorkspaceLeaf) {
-		super(leaf);
-		this.plugin = plugin;
-	}
-
-
-	getDisplayText(): string {
-		return 'Obsidian Supercharged - Latex';
-	}
-
-	getViewType(): string {
-		return LatexContextViewType;
-	}
-
-	async onload(): Promise<void> {
-		await loadMathJax();
-
-		if (!this.plugin.activeEditor) {
-			/*
-			this happens if the app gets closed and reopened
-			in this case, we want to close the leaf as we have no access to an editor.
-			this means that our button callbacks can't know where to insert text
-			*/
-			this.leaf.detach();
-			return;
-		}
-
-		const container = this.contentEl;
-
-		const rootEl = container.createDiv({ cls: 'supercharged-table' });
-
-		const commandGroups = [{
-			name: 'custom commands',
-			commands: this.plugin.settings.custom_commands
-		}].concat(COMMAND_GROUPS);
-
-		commandGroups.forEach((group, i) => {
-			const groupDiv= rootEl.createDiv();
-			const header = groupDiv.createEl('h2',{cls:'collapsible'});
-			header.textContent = group.name;
-			const content = groupDiv.createDiv({cls:'content'});
-			group.commands.forEach((command, index) => {
-				drawButton(command, content, () => {
-					this.plugin.insertText(this.plugin.activeEditor, command);
-					this.plugin.activeEditor.focus(); // return focus to editor after the button has been pressed
-				});
-			})
-		});
-		collapse();
-		await finishRenderMath();
-	}
-
-	onunload(): void {
-		this.plugin.latexLeaf = null;
-		this.plugin.canvasLeaf = null;
-	}
-}
-
-function drawButton(latexCommand: string, parent: HTMLElement, callback: () => any): HTMLButtonElement {
-	const button = parent.createEl('button');
-	button.appendChild(renderMath(latexCommand, true));
-	button.onClickEvent(callback);
-	return button;
-}
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
